9f856f51c8c6d09a3708d23e03ba2d89abba0313
RahulTirkey <rahul.tirkey303@gmail.com>
2020-10-23 21:58:09 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AirLineBookingComponent } from './AirLines/Components/air-line-booking/air-line-booking.component';
+import { AirLineMainBodyComponent } from './AirLines/Components/air-line-main-body/air-line-main-body.component';
+import { AirLineHeaderComponent } from './AirLines/Components/air-line-header/air-line-header.component';
+import { MainHomeComponent } from './main-home/main-home.component';
+import { CharterBookingComponent } from './AirLines/Components/charter-booking/charter-booking.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: '',
+    component: MainHomeComponent
+  },
+{
+  path: 'airline',
+  component: AirLineHeaderComponent,
+  children:[
+    {path:'',component:AirLineMainBodyComponent},
+    {path:'bookingAir',component:AirLineBookingComponent},
+    {path:'bookingCharter',component:CharterBookingComponent}
+     ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHomeComponent } from './main-home/main-home.component';
+import { AirLineHeaderComponent } from './AirLines/Components/air-line-header/air-line-header.component';
+import { AirLineMainBodyComponent } from './AirLines/Components/air-line-main-body/air-line-main-body.component';
+import { AirLineBookingComponent } from './AirLines/Components/air-line-booking/air-line-booking.component';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AirLineBookingService } from '../app/AirLines/Services/air-line-booking.service';
+import { CharterBookingComponent } from './AirLines/Components/charter-booking/charter-booking.component';
+import { CustomerComponent } from './Customer/customer/customer.component';
+import { RegisterCustomerComponent } from './Customer/register-customer/register-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHomeComponent,
+    AirLineHeaderComponent,
+    AirLineMainBodyComponent,
+    AirLineBookingComponent,
+    CharterBookingComponent,
+    CustomerComponent,
+    RegisterCustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AirLineBookingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
