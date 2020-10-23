cae149ff7cbc046c33c36df5023413e6a5594d7c
RahulTirkey <rahul.tirkey303@gmail.com>
2020-10-24 00:15:01 +0530

    Build Ready for Heroku1

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/baloo-app'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/baloo-app/index.html'));});
+    app.get('*', (req, res) => {
+        res.sendFile(`./baloo-app/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+    })});
 app.listen(process.env.PORT || 8080);
