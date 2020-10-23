3610c0d2e518567eadf3c57c26946162699dcad5
RahulTirkey <rahul.tirkey303@gmail.com>
2020-10-24 00:28:04 +0530

    Changes added1

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/baloo-app'));
-app.get('/*', function(req,res) {
-    app.get('*', (req, res) => {
+app.get('*', (req, res) => {
         res.sendFile(`./baloo-app/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-    })});
+    });
 app.listen(process.env.PORT || 8080);
