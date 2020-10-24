a9161a45409a6bef243790361933e474e1c3b7a6
RahulTirkey <rahul.tirkey303@gmail.com>
2020-10-24 11:44:00 +0530

    Changes added5

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app1 = express();
-app.use(express.static(__dirname + '/dist'));  
-app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
+app1.use(express.static(__dirname + 'dist/baloo-app'));  
+app1.all('/*', (req, res) => {  
+  res.status(200).sendFile(__dirname + 'dist/baloo-app/index.html');  
 });    
 app1.listen(process.env.PORT || 8080);
