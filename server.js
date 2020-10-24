607ba68e2697a58bec21a8b48dc3478fb58039dd
RahulTirkey <rahul.tirkey303@gmail.com>
2020-10-24 13:36:24 +0530

    Inital Commit

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
 const app = express();
- 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/BalooApp'));
