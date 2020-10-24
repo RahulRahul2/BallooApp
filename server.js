d29b25f1be49e8913c1d155ea8c1c1f5f2ebc176
RahulTirkey <rahul.tirkey303@gmail.com>
2020-10-24 13:02:11 +0530

    Changes adde6

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+ 
 const app = express();
-app.use(express.static(__dirname +'/dist/baloo-app'));  
-app.get('/*', (req, res) => {  
-  res.sendFile(__dirname +'/dist/baloo-app/index.html');  
-});    
+ 
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/baloo-app'));
+ 
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/baloo-app/index.html'));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
